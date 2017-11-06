a8434da0d188bf2a5bf8ba4ce2842ebbabc4029e
Rodney R <majid.muhassan@gmail.com>
2017-11-06 01:05:03 +0000

    just practicing git push and also want to log where I am. still a ways to go but VERY motivated

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// <!--Crystal Collector Game-->
+// <!--This game will contain instructions on how to play, output a random number,-->
+// <!--collect and recod the win to loss ratio, display 4 jewel images, and ouput -->
+// <!--a tally so the user will now how close they are to reaching the random number-->
+// <!--I also want to figure out how to add a timerfor a personal bonus-->
+// Each crystal will have a radom number assigned to it during the game.
+//  When the game starts over a new random number will be assigned.
+//  The computer will also select a new number at the start of each game.
+// The random number shown at the start of the game should be between 19 - 120.
+// Each crystal should have a random hidden value between 1 - 12.
+
+
+var randNum;
+var lost;
+var win;
